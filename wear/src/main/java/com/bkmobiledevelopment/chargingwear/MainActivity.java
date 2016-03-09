e8f2e1eba0cefb84d6410d8aee15262ff089f71b
kbrohkahn <kevin@broh-kahn.com>
2016-03-09 18:35:43 -0500

    Add mobile project so app can be installed.

---
+++ wear/src/main/java/com/bkmobiledevelopment/chargingwear/MainActivity.java
@@ -0,0 +1,133 @@
+package com.bkmobiledevelopment.chargingwear;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.wearable.view.WearableListView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements WearableListView.ClickListener {
+
+	private final String[] listItems = {"While " + Build.MODEL + " is charging...",
+			"Show black screen",
+			"Disable bluetooth"};
+	public static boolean[] listItemValues = {false, true, true};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		WearableListView listView = (WearableListView) findViewById(R.id.wearable_list);
+		listView.setAdapter(new Adapter(this, listItems));
+		listView.setClickListener(this);
+
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		final Resources resources = getResources();
+
+		listItemValues[1] = preferences.getBoolean(resources.getString(R.string.black_screen_key),
+				true);
+		listItemValues[2] = preferences.getBoolean(resources.getString(R.string.bluetooth_key),
+				true);
+	}
+
+	@Override
+	public void onClick(WearableListView.ViewHolder v) {
+		Integer position = (Integer) v.itemView.getTag();
+
+		if (position > 0) {
+			boolean newValue = !listItemValues[position];
+			listItemValues[position] = newValue;
+			int newImageResource = R.drawable.confirmation;
+			if (!newValue) {
+				newImageResource = R.drawable.deconfirmation;
+			}
+
+			((Adapter.ItemViewHolder) v).imageView.setImageResource(newImageResource);
+
+			int preferenceId;
+			switch (position) {
+				case 1:
+					preferenceId = R.string.black_screen_key;
+					break;
+				case 2:
+					preferenceId = R.string.bluetooth_key;
+					break;
+				default:
+					preferenceId = -1;
+					break;
+			}
+
+			SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this)
+					.edit();
+			editor.putBoolean(getResources().getString(preferenceId), newValue);
+			editor.apply();
+		}
+	}
+
+	@Override
+	public void onTopEmptyRegionClick() {
+	}
+
+	private static final class Adapter extends WearableListView.Adapter {
+		private String[] mDataset;
+		private final LayoutInflater mInflater;
+
+		public Adapter(Context context, String[] dataset) {
+			mInflater = LayoutInflater.from(context);
+			mDataset = dataset;
+		}
+
+		public static class ItemViewHolder extends WearableListView.ViewHolder {
+			private TextView textView;
+			private ImageView imageView;
+
+			public ItemViewHolder(View itemView) {
+				super(itemView);
+				textView = (TextView) itemView.findViewById(R.id.name);
+				imageView = (ImageView) itemView.findViewById(R.id.circle);
+			}
+		}
+
+		@Override
+		public WearableListView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			return new ItemViewHolder(mInflater.inflate(R.layout.wear_list_item, null));
+		}
+
+		@Override
+		public void onBindViewHolder(WearableListView.ViewHolder holder, final int position) {
+			final ItemViewHolder itemHolder = (ItemViewHolder) holder;
+
+			itemHolder.textView.setText(mDataset[position]);
+
+			if (position > 0) {
+				itemHolder.imageView.setVisibility(View.VISIBLE);
+
+				int imageResource = R.drawable.confirmation;
+				if (!listItemValues[position]) {
+					imageResource = R.drawable.deconfirmation;
+				}
+				itemHolder.imageView.setImageResource(imageResource);
+			} else {
+				itemHolder.imageView.setVisibility(View.GONE);
+			}
+
+			holder.itemView.setTag(position);
+		}
+
+		@Override
+		public int getItemCount() {
+			return mDataset.length;
+		}
+	}
+}
---
+++ wear/src/main/java/com/bkmobiledevelopment/chargingwear/PowerDisconnectedReceiver.java
@@ -0,0 +1,32 @@
+package com.bkmobiledevelopment.chargingwear;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by kbrohkahn on 3/8/2016.
+ * Receiver listening for charge status change
+ */
+public class PowerDisconnectedReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+		final Resources resources = context.getResources();
+
+		boolean bluetooth = preferences.getBoolean(resources.getString(R.string.bluetooth_key),
+				true);
+
+		if (bluetooth) {
+			BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+			if (!mBluetoothAdapter.isEnabled()) {
+				mBluetoothAdapter.enable();
+			}
+		}
+	}
+}
